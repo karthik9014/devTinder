07195c7acfcb87193af1d110a541a8baa5584a89
Karthik <90187061+karthik9014@users.noreply.github.com>
2025-03-02 14:13:12 +0000

    Notes

--- src/app.js
+++ src/app.js
@@ -4,17 +4,12 @@ const app = express();
 
 app.listen(7000, () => console.log("Running in 7000"));
 
-app.use("/user",(req, res) => {
-    res.send("User Checked Succesfully")
-})
-app.delete("/user",(req, res) => {
-    res.send("User DELETE call Succesful")
-})
-app.get("/user",(req, res) => {
-    res.send("User GET call Succesful")
-})
-app.post("/user",(req, res) => {
-    res.send("User POST call Succesful")
-})
-
+app.use("/admin", (req, res, next) => {
+    throw new Error("Something went wrong");
+});
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        res.status(500).send(err.message);
+    }
+});
 
