d7e26542ca61ef9005599ed7bee4f1b4fba6d0c8
Karthik <90187061+karthik9014@users.noreply.github.com>
2025-03-02 11:20:12 +0000

    Second Commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 
 const app = express();
-app.use((req, res) => {
-    res.send("Hello World")
-})
 
 app.listen(7000, () => console.log("Running in 7000"));
+
+app.use("/user",(req, res) => {
+    res.send("User Checked Succesfully")
+})
+app.delete("/user",(req, res) => {
+    res.send("User DELETE call Succesful")
+})
+app.get("/user",(req, res) => {
+    res.send("User GET call Succesful")
+})
+app.post("/user",(req, res) => {
+    res.send("User POST call Succesful")
+})
+
 
